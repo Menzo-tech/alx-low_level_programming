efedd22dccc8dfbd0d23e56751819b9aeadd0afc
Mmenim <inyangmmenim@gmail.com>
2022-10-19 17:31:22 -0700

    even value fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - sum of fibonacci even value
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int a1, a2, fn, afn;
+
+	a1 = 1;
+	a2 = 2;
+	fn = afn = 0;
+	while (fn <= 4000000)
+	{
+		fn = a1 + a2;
+		a1 = a2;
+		a2 = fn;
+		if (n1 % 2 == 0)
+		{
+			afn += a1;
+		}
+	}
+	printf("%ld\n", afn);
+	return (0);
+}
