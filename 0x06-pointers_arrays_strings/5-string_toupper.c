1f2fa8db5a8c187e972b74f885aece1b9d38acfe
Mmenim <inyangmmenim@gmail.com>
2022-10-27 07:16:49 -0700

    uppercase string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase to uppercase string
+ * @s: input string
+ * Return: the pointer to dest
+ */
+char *string_toupper(char *s)
+{
+	int poll = 0;
+
+	while (s[poll] != '\0')
+	{
+		if ((s[poll] >= a) && (s[poll] < z))
+			s[poll] = s[poll] - 32;
+		poll++;
+	}
+
+	return (s);
+}
