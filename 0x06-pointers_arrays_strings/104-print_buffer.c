cdce54313c50902208b08abb725d66b0c0cf13af
Mmenim <inyangmmenim@gmail.com>
2022-10-30 18:12:49 -0700

    print the buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 /**
- * printable_ASCII - determines if n is a printable ASCII char
+ * isPrintableASCII - determines if n is a printable ASCII char
  * @n: integer
  * Return: 1 if true, otherwise 0
  */
-int printable_ASCII(int n)
+int isPrintableASCII(int n)
 {
 	return (n >= 32 && n <= 126);
 }
 
 /**
- * print_hexes - print hex value for string b in formatted form
+ * printHexes - print hex value for string b in formatted form
  * @b: string to print
  * @start: starting position
  * @end: ending position
  */
-void print_hexes(char *b, int start, int end)
+void printHexes(char *b, int start, int end)
 {
 	int i = 0;
 
@@ -34,20 +34,20 @@ void print_hexes(char *b, int start, int end)
 }
 
 /**
- * print_ASCII - print ASCII value for string b
+ * printASCII - print ASCII value for string b
  * formatted to replace nonprintable char with '.'
  * @b: string to print
  * @start: starting position
  * @end: ending position
  */
-void print_ASCII(char *b, int start, int end)
+void printASCII(char *b, int start, int end)
 {
 	int ch, i = 0;
 
 	while (i < end)
 	{
 		ch = *(b + i + start);
-		if (!printable_ASCII(ch))
+		if (!isPrintableASCII(ch))
 			ch = 46;
 		printf("%c", ch);
 		i++;
@@ -69,8 +69,8 @@ void print_buffer(char *b, int size)
 		{
 			end = (size - start < 10) ? size - start : 10;
 			printf("%08x: ", start);
-			print_hexes(b, start, end);
-			print_ASCII(b, start, end);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
 			printf("\n");
 		}
 	}
