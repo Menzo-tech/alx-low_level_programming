4f4292dfca3608b680e54068c232e8aedb254113
Mmenim <inyangmmenim@gmail.com>
2022-11-01 02:32:44 -0700

    character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strchr - locate a character in a string
+ * @s: string to search
+ * @c: char of string
+ * Return: a pointer to first occurence of char
+ * else NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	while (1)
+	{
+		a = (*s)++;
+		if (a == c)
+		{
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
+}
