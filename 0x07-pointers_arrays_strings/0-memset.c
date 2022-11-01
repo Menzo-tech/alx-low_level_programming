d19cda7d24740eaa205f24faadd30b60eb3129e2
Mmenim <inyangmmenim@gmail.com>
2022-10-31 17:30:12 -0700

    memory set

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - fills the memory area
+ * @s: memory area to be filled
+ * @b: number of bytes
+ * @n: bytes
+ * Return: pointer to memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
