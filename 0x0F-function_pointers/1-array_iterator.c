dbb636fba94f0c72433a8740d0c76fc2ddd301df
Mmenim <inyangmmenim@gmail.com>
2022-11-16 22:27:35 -0800

    solution one

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointer.h"
+
+/**
+ * array_iterator - executes a function
+ * @array: array to iterate
+ * @size: size of the array
+ * @action: pointer to function used
+ * Return: no return
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
