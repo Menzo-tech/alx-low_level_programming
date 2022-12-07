4a6720bcc5bfabea38682ec6b531fbfb9fc6b085
Menzo-tech <113693329+Menzo-tech@users.noreply.github.com>
2022-12-07 13:23:01 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,3 +3,13 @@
 void print_statement(void) __attribute__ ((constructor));
 
 /**
+ * print_statement - prints a statement before main function
+ *
+ * Return: Nothing
+ */
+
+void print_statement(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
